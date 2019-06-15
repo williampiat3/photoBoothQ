4025ad267708ce2b25829845d761b756e35f1c67
williampiat3 <william.piat3@gmail.com>
2019-06-16 00:33:51 +0200

    new interface

--- interface.py
+++ interface.py
@@ -3,4 +3,10 @@ from pygame.locals import *
 
 if __name__ == "__main__":
 	pygame.init()
-	fenetre = pygame.display.set_mode((640, 480))
+	#opening window
+	window = pygame.display.set_mode((640, 480))
+	keep_open = True
+	#Main loop for running the program
+	while keep_open:
+		continue
+	
