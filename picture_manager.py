90c992a1a4218050ab3b306f35d70db06b7a3424
lejuj <lejuj@DESKTOP-IRNK26C>
2019-06-23 00:01:10 +0200

    time lapse function implemented for picamera

--- picture_manager.py
+++ picture_manager.py
@@ -1,5 +1,6 @@
 from picamera import PiCamera
 from time import sleep
+from os.path import join
 
 
 class PictureManager(object):
@@ -67,3 +68,22 @@ class PictureManager(object):
         sleep(time)
         self.camera.stop_recording()
         self.camera.stop_preview()
+        
+    def time_lapse(self, path, lapse_minute, interval=10, file_name="lapse"):
+        '''
+        method taking a time lapse during a given time
+        :param path: full path of the created pictures, no extension or filename needed
+        :param lapse_minute: time in minutes of the time lapse
+        :param interval: interval between two picture, must be more than 5 seconds
+        :param file_name: name of the files generated, no need for extension
+        '''
+        if interval <= 5:
+            msg = "the interval between two picture must be more than 5 seconds"
+            raise Exception(msg)
+        
+        number_picture = lapse_minute * 60 / interval
+        corrected_interval = interval - 5;
+        for picture in range(number_picture):
+            sleep(corrected_interval)
+            self.take_picture(join(path, file_name + str(picture) + ".jpg"), 5)
+            
