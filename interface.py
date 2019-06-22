f8a8534916e20df0556ecda1e86cb2e0eedcd7ea
will <william.piat3@gmail.com>
2019-06-22 23:22:05 +0200

    sounds converted

--- interface.py
+++ interface.py
@@ -3,11 +3,12 @@ from pygame.locals import *
 import time
 import random
 
+
 def pick_random_in_list(list_item):
 	return list_item[random.randint(0,len(list_item)-1)]
 
 #function to load a batch of screens 
-def load_batch_images(*args):
+def load_batch_images(*args,size_screen=(1058, 595)):
 	return [pygame.transform.scale(pygame.image.load(arg),(1058, 595)) for arg in args]
 
 #function to load a batch of sounds
@@ -16,21 +17,41 @@ def load_sounds(*args):
 	return sounds
 if __name__ == "__main__":
 	pygame.init()
+	#constants:
+	#Size Screen
+	size_screen=(1058, 595)
+
+	#Sounds
 	countdown_sound = load_sounds("sound_booth/countdown.ogg")[0]
-	picture_sounds = load_sounds("sound_booth/camera-shutter1.ogg","sound_booth/camera-shutter3.ogg","sound_booth/camera-shutter8.ogg","sound_booth/chinese-gong.ogg","sound_booth/Cuckoo-clock.ogg","sound_booth/party_horn.ogg")
-	#opening window
-	window = pygame.display.set_mode((1058, 595),RESIZABLE)
-	window.fill([255,255,255])
-	pygame.display.flip()
-	#loading different screens 
-	welcomes = load_batch_images("picture_camera/ap_right.png","picture_camera/ap.png","picture_camera/ap_left.png")
-	readies = load_batch_images("picture_camera/ready.png")
+
+	picture_sounds = load_sounds("sound_booth/camera-shutter1.ogg",
+								"sound_booth/camera-shutter3.ogg",
+								"sound_booth/camera-shutter8.ogg",
+								"sound_booth/chinese-gong.ogg",
+								"sound_booth/Cuckoo-clock.ogg",
+								"sound_booth/party_horn.ogg")
+	
+	#Pictures
+	welcomes = load_batch_images("picture_camera/ap_right.png",
+								"picture_camera/ap.png",
+								"picture_camera/ap_left.png",
+								size_screen=size_screen)
+
+	readies = load_batch_images("picture_camera/ready.png",
+								size_screen=size_screen)
 
 	#reverse count screens 
-	screens_countdown = load_batch_images(*["count/image-000{:02}.png".format(i) for i in range(1,52)])
+	screens_countdown = load_batch_images(*["count/image-000{:02}.png".format(i) for i in range(1,52)],
+											size_screen=size_screen)
 
 	#ending screens
-	screens_ending = load_batch_images("picture_camera/end1.png")
+	screens_ending = load_batch_images("picture_camera/end1.png",
+										size_screen=size_screen)
+
+	#opening window with black screen
+	window = pygame.display.set_mode(size_screen,RESIZABLE)
+	window.fill([0,0,0])
+	pygame.display.flip()
 
 	#setting first screen as the centered one
 	window.blit(welcomes[1],(0,0))
@@ -64,7 +85,8 @@ if __name__ == "__main__":
 			if event.type == MOUSEBUTTONDOWN:
 				#displaying ready message
 				countdown_sound.play()
-				window.blit(readies[0],(0,0))
+				
+				window.blit(pick_random_in_list(readies[0]),(0,0))
 				pygame.display.flip()
 				time.sleep(5)
 				#erasing 
